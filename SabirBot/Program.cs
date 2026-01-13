837342f789856d24ec5ad0714ebf47fb1c97356b
MISHAKALINA <sobadogsgame@gmail.com>
2026-01-13 18:13:41 +0300

    qwe

--- SabirBot/Program.cs
+++ SabirBot/Program.cs
@@ -20,6 +20,7 @@ namespace SabirBot
                 HandleErrorAsync,
                 cancellationToken: cts.Token
             );
+            var me = await bot.GetMe();
             Console.WriteLine($"@{me.Username} started");
             var me = await bot.GetMe();
         }
